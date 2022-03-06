1a3b87c3f66c96b259969ed12bf16d18d5309566
Fabrizio Boco <fabrizio.boco@gmail.com>
2022-03-06 16:31:22 +0100

    Fix to Open and Close button color

--- Sources/SwiftuiComponents/Menu/ContainerView.swift
+++ Sources/SwiftuiComponents/Menu/ContainerView.swift
@@ -69,7 +69,7 @@ public struct ContainerView: View {
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .font(.title.bold())
-                .foregroundColor(.black)
+                .foregroundColor(Color(uiColor: .label))
                 .frame(width: controller.openButtonSize, height: controller.openButtonSize)
         }
         .opacity(controller.showMenu ? 0 : 1)
@@ -84,7 +84,7 @@ public struct ContainerView: View {
         } label: {
             Image(systemName: "xmark")
                 .font(.title.bold())
-                .foregroundColor(.black)
+                .foregroundColor(Color(uiColor: .label))
         }
         .opacity(controller.showMenu ? 1 : 0)
         .padding()
