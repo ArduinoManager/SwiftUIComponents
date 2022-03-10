c3d176b1278222759849e1a8c68e4765796ffff4
Fabrizio Boco <fabrizio.boco@gmail.com>
2022-03-10 18:46:00 +0100

    Menu For macOS

--- Sources/SwiftUIComponents/Menu/Menu.swift
+++ Sources/SwiftUIComponents/Menu/Menu.swift
@@ -14,10 +14,82 @@ public struct Menu: View {
 
     public init(controller: MenuController) {
         self.controller = controller
-        UITabBar.appearance().isHidden = true
+        #if os(iOS)
+            UITabBar.appearance().isHidden = true
+        #endif
     }
 
     public var body: some View {
+        #if os(macOS)
+        
+        NavigationView {
+            List {
+                NavigationLink(destination: ContentView()) {
+                    Label("Welcome", systemImage: "star")
+                }
+                
+                Spacer()
+                
+                Text("DASHBOARD")
+                    .font(.system(size: 10))
+                    .fontWeight(.bold)
+                Group{
+                    NavigationLink(destination: ContentView()) {
+                        Label("Home", systemImage: "house")
+                    }
+                    NavigationLink(destination: ContentView()) {
+                        Label("Websites", systemImage: "globe")
+                    }
+                    NavigationLink(destination: ContentView()) {
+                        Label("Domains", systemImage: "link")
+                    }
+                    NavigationLink(destination: ContentView()) {
+                        Label("Templates", systemImage: "rectangle.stack")
+                    }
+                }
+                
+                Spacer()
+                
+                Text("PROFILE")
+                    .font(.system(size: 10))
+                    .fontWeight(.bold)
+                Group {
+                    NavigationLink(destination: ContentView()) {
+                        Label("My Account", systemImage: "person")
+                    }
+                    NavigationLink(destination: ContentView()) {
+                        Label("Notifications", systemImage: "bell")
+                    }
+                    NavigationLink(destination: ContentView()) {
+                        Label("Settings", systemImage: "gear")
+                    }
+                }
+                
+                Spacer()
+                
+                Divider()
+                NavigationLink(destination: ContentView()) {
+                    Label("Sign Out", systemImage: "arrow.backward")
+                }
+            }
+            .listStyle(SidebarListStyle())
+            .navigationTitle("Explore")
+            .frame(minWidth: 150, idealWidth: 250, maxWidth: 300)
+            .padding([.leading], 0)
+            .toolbar {
+                ToolbarItem(placement: .navigation) {
+                    Button(action: toggleSidebar, label: {
+                        Image(systemName: "sidebar.left")
+                    })
+                }
+            }
+            
+            ContentView()
+        }
+    
+        
+        #endif
+        #if os(iOS)
         ZStack {
             // Side Menu
             SideMenuView(controller: controller)
@@ -29,6 +101,7 @@ public struct Menu: View {
                 .offset(x: controller.showMenu ? getRect().width / 2 : 0)
                 .ignoresSafeArea()
         }
+        #endif
     }
 }
 
