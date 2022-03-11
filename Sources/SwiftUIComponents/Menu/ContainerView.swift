53fa4db5f63e97014bab4aad02b30260a9889527
Fabrizio Boco <fabrizio.boco@gmail.com>
2022-03-11 10:48:44 +0100

    Menu for macOS

--- Sources/SwiftUIComponents/Menu/ContainerView.swift
+++ Sources/SwiftUIComponents/Menu/ContainerView.swift
@@ -8,110 +8,111 @@
 import SwiftUI
 
 #if os(iOS)
-public struct ContainerView: View {
-    @ObservedObject var controller: MenuController
-    @State private var orientation = UIDeviceOrientation.unknown
-    
-    public var body: some View {
-        Print(Self._printChanges())
-        VStack(spacing: 0) {
-            if controller.openButtonAtTop {
-                HStack(spacing: 0) {
-                    OpenButton()
-                    Spacer()
-                    if controller.titleView != nil {
-                        controller.titleView
+    public struct ContainerView: View {
+        @ObservedObject var controller: MenuController
+        @State private var orientation = UIDeviceOrientation.unknown
+
+        public var body: some View {
+            Print(Self._printChanges())
+            VStack(spacing: 0) {
+                if controller.openButtonAtTop {
+                    HStack(spacing: 0) {
+                        OpenButton()
+                        Spacer()
+                        if controller.titleView != nil {
+                            controller.titleView
+                        }
+                    }
+                    .padding([.horizontal], isLandscape() ? 40 : 20)
+                    .padding(.bottom)
+                    .padding(.top, isLandscape() ? 10.0 : getSafeArea().top)
+                }
+
+                TabView(selection: $controller.currentTab) {
+                    ForEach(controller.menuItems, id: \.self) { item in
+                        item.makeView()
+                            .tag(item.title)
                     }
                 }
-                .padding([.horizontal], isLandscape() ? 40 : 20)
-                .padding(.bottom)
-                .padding(.top, isLandscape() ? 10.0 : getSafeArea().top)
-            }
-
-            TabView(selection: $controller.currentTab) {
-                ForEach(controller.menuItems, id: \.self) { item in
-                    item.makeView()
-                        .tag(item.title)
-                }
-            }
-            .onChange(of: controller.currentTab, perform: { _ in
-                if controller.autoClose {
-                    withAnimation(.spring()) {
-                        controller.showMenu = false
+                .onChange(of: controller.currentTab, perform: { _ in
+                    if controller.autoClose {
+                        withAnimation(.spring()) {
+                            controller.showMenu = false
+                        }
                     }
-                }
-            })
+                })
 
-            if !controller.openButtonAtTop {
-                HStack(spacing: 0) {
-                    OpenButton()
-                    Spacer()
-                    if controller.titleView != nil {
-                        controller.titleView
+                if !controller.openButtonAtTop {
+                    HStack(spacing: 0) {
+                        OpenButton()
+                        Spacer()
+                        if controller.titleView != nil {
+                            controller.titleView
+                        }
                     }
+                    .padding([.horizontal])
+                    .padding(.top)
+                    .padding(.bottom, getSafeArea().bottom)
                 }
-                .padding([.horizontal])
-                .padding(.top)
-                .padding(.bottom, getSafeArea().bottom)
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .overlay(CloseButton(), alignment: .topLeading)
+            .background(controller.titleViewBackgroundColor)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .overlay(CloseButton(), alignment: .topLeading)
-        .background(controller.titleViewBackgroundColor)
-    }
 
-    @ViewBuilder
-    func OpenButton() -> some View {
-        Button {
-            withAnimation(.spring()) {
-                controller.showMenu.toggle()
+        @ViewBuilder
+        func OpenButton() -> some View {
+            Button {
+                withAnimation(.spring()) {
+                    controller.showMenu.toggle()
+                }
+            } label: {
+                Image(systemName: controller.openButtonIcon)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .font(.title.bold())
+                    .foregroundColor(controller.openButtonColor)
+                    .frame(width: controller.openButtonSize, height: controller.openButtonSize)
             }
-        } label: {
-            Image(systemName: controller.openButtonIcon)
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .font(.title.bold())
-                .foregroundColor(controller.openButtonColor)
-                .frame(width: controller.openButtonSize, height: controller.openButtonSize)
+            .opacity(controller.showMenu ? 0 : 1)
         }
-        .opacity(controller.showMenu ? 0 : 1)
-    }
 
-    @ViewBuilder
-    func CloseButton() -> some View {
-        Button {
-            withAnimation(.spring()) {
-                controller.showMenu.toggle()
+        @ViewBuilder
+        func CloseButton() -> some View {
+            Button {
+                withAnimation(.spring()) {
+                    controller.showMenu.toggle()
+                }
+            } label: {
+                Image(systemName: "xmark")
+                    .font(.title.bold())
+                    .foregroundColor(controller.openButtonColor)
             }
-        } label: {
-            Image(systemName: "xmark")
-                .font(.title.bold())
-                .foregroundColor(controller.openButtonColor)
+            .opacity(controller.showMenu ? 1 : 0)
+            .padding()
+            .padding(.top)
         }
-        .opacity(controller.showMenu ? 1 : 0)
-        .padding()
-        .padding(.top)
-    }
 
-    func isLandscape() -> Bool {
-        #if os(iOS)
-        return UIDevice.current.orientation == .landscapeLeft || UIDevice.current.orientation == .landscapeRight
-        #endif
+        func isLandscape() -> Bool {
+            #if os(iOS)
+                return UIDevice.current.orientation == .landscapeLeft || UIDevice.current.orientation == .landscapeRight
+            #endif
+            #if os(macOS)
+                return true
+            #endif
+        }
     }
-}
 #endif
-
 
 #if os(macOS)
 
-public struct ContainerView: View {
-    @ObservedObject var controller: MenuController
-    
-    public var body: some View {
-        Text("ContainerView")
+    public struct ContainerView: View {
+        @ObservedObject var controller: MenuController
+
+        public var body: some View {
+            Text("ContainerView")
+        }
     }
-    
-}
 
 #endif
 
@@ -127,7 +128,7 @@ struct MainViewContainer: View {
             TabMenuSpacer(height: 50),
             TabMenuItem(title: "Profile", systemIcon: "person.fill", view: AnyView(TestView(text: "Profile").background(.green))),
             TabMenuItem(title: "Profile2",
-                        icon:Image("logo", bundle: .module),
+                        icon: Image("logo", bundle: .module),
                         view: AnyView(TestView(text: "Profile").background(.green))),
 
             TabMenuDivider(color: .white),
@@ -138,14 +139,14 @@ struct MainViewContainer: View {
             HandlerMenuItem(title: "Logout", systemIcon: "rectangle.portrait.and.arrow.right") {
                 print("Logout")
             },
-            
+
             TabMenuDivider(color: .white),
             HandlerMenuItem(title: "Kill!", icon: Image("logo", bundle: .module)) {
                 print("Login")
             },
         ],
         sideTitleView: AnyView(SideTitleView()
-                              ), backgroundColor: .black, itemsColor: .red
+        ), backgroundColor: .black, itemsColor: .red
     )
 
     var body: some View {
@@ -166,6 +167,7 @@ struct SideTitleView: View {
             Text("Fuck !")
                 .font(.title2.bold())
                 .foregroundColor(.white)
+                .frame(height: 45, alignment: .trailing)
         }
         .padding()
         .background(.green)
--- Sources/SwiftUIComponents/Menu/SideMenuView.swift
+++ Sources/SwiftUIComponents/Menu/SideMenuView.swift
@@ -7,18 +7,24 @@
 
 import SwiftUI
 
-
-#if os(iOS)
 public struct SideMenuView: View {
     @ObservedObject var controller: MenuController
     @Namespace var animation
+    #if os(iOS)
+        let buttonHeight: CGFloat = 48.0
+    #endif
+    #if os(macOS)
+        let buttonHeight: CGFloat = 30.0
+    #endif
 
     public var body: some View {
         VStack {
             if controller.sideTitleView != nil {
                 HStack {
                     controller.sideTitleView
+                    #if os(iOS)
                         .frame(maxWidth: getRect().width / 2, alignment: .leading)
+                    #endif
                     Spacer()
                 }
             } else {
@@ -44,8 +50,12 @@ public struct SideMenuView: View {
                         case is TabMenuDivider:
                             let i = item as! TabMenuDivider
                             Divider()
+                            #if os(iOS)
                                 .background(i.color != nil ? i.color! : Color(uiColor: .label))
-
+                            #endif
+                            #if os(macOS)
+                                .background(i.color != nil ? i.color! : Color(NSColor.labelColor))
+                            #endif
                         default:
                             EmptyView()
                         }
@@ -55,15 +65,20 @@ public struct SideMenuView: View {
             }
             .padding()
             .padding(.top, 10)
-            .frame(width: getRect().width / 2, alignment: .leading)
+            #if os(iOS)
+                .frame(width: getRect().width / 2, alignment: .leading)
+            #endif
+
             .frame(maxWidth: .infinity, alignment: .leading)
         }
         .padding(.leading, 10)
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
         .background(controller.backgroundColor)
-        .onRotate { _ in
-            controller.objectWillChange.send() // Force redraw!
-        }
+        #if os(iOS)
+            .onRotate { _ in
+                controller.objectWillChange.send() // Force redraw!
+            }
+        #endif
     }
 
     @ViewBuilder
@@ -77,7 +92,7 @@ public struct SideMenuView: View {
             HStack {
                 makeImage(item: item)
                     .font(.title3)
-                    .frame(width: controller.currentTab == item.title ? 48 : nil, height: 48)
+                    .frame(width: controller.currentTab == item.title ? buttonHeight : nil, height: buttonHeight)
                     .foregroundColor(controller.currentTab == item.title ? controller.selectedItemBackgroundColor : controller.itemsColor)
                     .background(
                         ZStack {
@@ -107,7 +122,12 @@ public struct SideMenuView: View {
                 }
             )
         }
-        .offset(x: controller.currentTab == item.title ? 15 : 0)
+        #if os(iOS)
+            .offset(x: controller.currentTab == item.title ? 15 : 0)
+        #endif
+        #if os(macOS)
+            .buttonStyle(PlainButtonStyle())
+        #endif
     }
 
     @ViewBuilder
@@ -121,7 +141,7 @@ public struct SideMenuView: View {
             HStack {
                 makeImage(item: item)
                     .font(.title3)
-                    .frame(width: controller.currentTab == item.title ? 48 : nil, height: 48)
+                    .frame(width: controller.currentTab == item.title ? buttonHeight : nil, height: buttonHeight)
                     .foregroundColor(controller.currentTab == item.title ? controller.selectedItemBackgroundColor : controller.itemsColor)
                     .background(
                         ZStack {
@@ -151,10 +171,14 @@ public struct SideMenuView: View {
                 }
             )
         }
-        .offset(x: controller.currentTab == item.title ? 15 : 0)
+        #if os(iOS)
+            .offset(x: controller.currentTab == item.title ? 15 : 0)
+        #endif
+        #if os(macOS)
+            .buttonStyle(PlainButtonStyle())
+        #endif
     }
-    
-    
+
     @ViewBuilder
     func makeImage(item: MenuItem) -> some View {
         if item.icon != nil {
@@ -162,85 +186,81 @@ public struct SideMenuView: View {
                 .resizable()
                 .scaledToFit()
                 .frame(width: 22, height: 22)
-        }
-        else {
+        } else {
             Image(systemName: item.systemIcon!)
         }
     }
-    
 }
-
-#endif
 
 #if os(macOS)
-public struct SideMenuView: View {
-    @ObservedObject var controller: MenuController
-    @Namespace var animation
-
-    public var body: some View {
-        
-        List {
-            NavigationLink(destination: ContentView()) {
-                Label("Welcome", systemImage: "star")
-            }
-            
-            Spacer()
-            
-            Text("DASHBOARD")
-                .font(.system(size: 10))
-                .fontWeight(.bold)
-            Group{
-                NavigationLink(destination: ContentView()) {
-                    Label("Home", systemImage: "house")
-                }
-                NavigationLink(destination: ContentView()) {
-                    Label("Websites", systemImage: "globe")
-                }
-                NavigationLink(destination: ContentView()) {
-                    Label("Domains", systemImage: "link")
-                }
-                NavigationLink(destination: ContentView()) {
-                    Label("Templates", systemImage: "rectangle.stack")
-                }
-            }
-            
-            Spacer()
-            
-            Text("PROFILE")
-                .font(.system(size: 10))
-                .fontWeight(.bold)
-            Group {
-                NavigationLink(destination: ContentView()) {
-                    Label("My Account", systemImage: "person")
-                }
-                NavigationLink(destination: ContentView()) {
-                    Label("Notifications", systemImage: "bell")
-                }
-                NavigationLink(destination: ContentView()) {
-                    Label("Settings", systemImage: "gear")
-                }
-            }
-            
-            Spacer()
-            
-            Divider()
-            NavigationLink(destination: ContentView()) {
-                Label("Sign Out", systemImage: "arrow.backward")
-            }
-        }
-        .listStyle(SidebarListStyle())
-        .navigationTitle("Explore")
-        .frame(minWidth: 150, idealWidth: 250, maxWidth: 300)
-        .padding([.leading], 0)
-        .toolbar {
-            ToolbarItem(placement: .navigation) {
-                Button(action: toggleSidebar, label: {
-                    Image(systemName: "sidebar.left")
-                })
-            }
-        }
-    }
-}
+    // public struct SideMenuView: View {
+//    @ObservedObject var controller: MenuController
+//    @Namespace var animation
+//
+//    public var body: some View {
+//
+//        List {
+//            NavigationLink(destination: ContentView()) {
+//                Label("Welcome", systemImage: "star")
+//            }
+//
+//            Spacer()
+//
+//            Text("DASHBOARD")
+//                .font(.system(size: 10))
+//                .fontWeight(.bold)
+//            Group{
+//                NavigationLink(destination: ContentView()) {
+//                    Label("Home", systemImage: "house")
+//                }
+//                NavigationLink(destination: ContentView()) {
+//                    Label("Websites", systemImage: "globe")
+//                }
+//                NavigationLink(destination: ContentView()) {
+//                    Label("Domains", systemImage: "link")
+//                }
+//                NavigationLink(destination: ContentView()) {
+//                    Label("Templates", systemImage: "rectangle.stack")
+//                }
+//            }
+//
+//            Spacer()
+//
+//            Text("PROFILE")
+//                .font(.system(size: 10))
+//                .fontWeight(.bold)
+//            Group {
+//                NavigationLink(destination: ContentView()) {
+//                    Label("My Account", systemImage: "person")
+//                }
+//                NavigationLink(destination: ContentView()) {
+//                    Label("Notifications", systemImage: "bell")
+//                }
+//                NavigationLink(destination: ContentView()) {
+//                    Label("Settings", systemImage: "gear")
+//                }
+//            }
+//
+//            Spacer()
+//
+//            Divider()
+//            NavigationLink(destination: ContentView()) {
+//                Label("Sign Out", systemImage: "arrow.backward")
+//            }
+//        }
+//        .listStyle(SidebarListStyle())
+//        .navigationTitle("Explore")
+//        .frame(minWidth: 150, idealWidth: 250, maxWidth: 300)
+//        .padding([.leading], 0)
+//        .toolbar {
+//            ToolbarItem(placement: .navigation) {
+//                Button(action: toggleSidebar, label: {
+//                    Image(systemName: "sidebar.left")
+//                })
+//            }
+//        }
+//    }
+    // }
 #endif
 
 struct SideMenu_Previews: PreviewProvider {
