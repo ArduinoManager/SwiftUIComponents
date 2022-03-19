796923c13703967a6fa52b5d70d73f0338a633d1
Fabrizio Boco <fabrizio.boco@gmail.com>
2022-03-19 11:02:32 +0100

    !

--- Sources/SwiftUIComponents/Menu/Menu.swift
+++ Sources/SwiftUIComponents/Menu/Menu.swift
@@ -30,7 +30,6 @@ public struct Menu: View {
                 SideMenuView(controller: controller)
                 // Right Panel
                 ContainerView(controller: controller, item: controller.menuItems[0])
-                }
             }
         #endif
         #if os(iOS)
