6af68e28bab8aeeb2b767144c566b1c6defb457b
Fabrizio Boco <fabrizio.boco@gmail.com>
2022-03-28 12:47:58 +0200

    !

--- Sources/SwiftUIComponents/SuperController.swift
+++ Sources/SwiftUIComponents/SuperController.swift
@@ -18,4 +18,20 @@ public class SuperController: Codable {
     init(type: ControllerType) {
         self.type = type
     }
+    
+    // MARK: - Encodable & Decodable
+    
+    enum CodingKeys: CodingKey {
+        case type
+    }
+    
+    required public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        type = try container.decode(ControllerType.self, forKey: .type)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(type, forKey: .type)
+    }
 }
